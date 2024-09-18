305986f78317cc8a0cc6f9d67db1701521eaf89f
Cal Clark <87346400+ItsCal7@users.noreply.github.com>
2024-09-18 16:21:47 -0500

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,54 @@
+﻿namespace JBU_RPG_Project;
+
+public class Character
+{
+    private string name;
+    private static int _head;
+    private static int _heart;
+    private static int _hand;
+
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+
+    public int Head
+    {
+        get => _head;
+        set
+        {
+            _head = value;
+        }
+    }
+    
+    public int Heart
+    {
+        get => _heart;
+        set
+        {
+            _heart = value;
+        }
+    }
+    
+    public int Hand
+    {
+        get => _hand;
+        set
+        {
+            _hand = value;
+        }
+    }
+    
+    public virtual void Attack(Character target)
+    {
+        if (_hand > target.Heart)
+        {
+            target.Heart -= _hand;
+        }
+        else
+        {
+            Console.WriteLine(name + " tried to attack " + target.Name + ", but missed");
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,53 @@
+﻿namespace JBU_RPG_Project;
+
+public class Player:Character
+{
+    public SortedSet<string> _backpack = new SortedSet<string>();
+    private static int _level;
+    private static int _xp;
+    private static string _weapon;
+    private static string _armour;
+
+    public void player()
+    {
+        _level = 1;
+        _xp = 0;
+        _weapon = "Fist";
+        _armour = "To-Go Box";
+        Head = 1;
+        Heart = 1;
+        Hand = 1;
+    }
+    
+    public int XP
+    {
+        get => _xp;
+        set
+        {
+            _xp = value;
+            CalculateLevel(_xp);
+        }
+    }
+
+    private static void CalculateLevel(int xp)
+    {
+        _xp = xp / 100;
+    }
+
+    public static void Equip(string item)
+    {
+        _item = item;
+    }
+    
+    public override void Attack(Character target)
+    {
+        if (Hand > target.Heart)//add weapon mod
+        {
+            target.Heart -= Hand;
+        }
+        else
+        {
+            Console.WriteLine(Name + " tried to attack " + target.Name + ", but missed");
+        }
+    }
+}
